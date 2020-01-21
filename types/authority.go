15115f92f1c036a682f847717dcd51f7a8882382
nghiatc <congnghia0609@gmail.com>
2020-01-21 12:20:30 +0700

    clean code

--- types/authority.go
+++ types/authority.go
@@ -17,20 +17,12 @@ func (auth *Authority) MarshalTransaction(encoder *transaction.Encoder) error {
 	enc.EncodeNumber(uint32(auth.WeightThreshold))
 	// encode AccountAuths as map[string]uint16
 	enc.EncodeUVarint(uint64(len(auth.AccountAuths)))
-	acc_keys := []string{}
-	for acc_key := range auth.AccountAuths {
-		acc_keys = append(acc_keys, acc_key)
-	}
 	for k, v := range auth.AccountAuths {
 		enc.EncodeString(k)
 		enc.EncodeNumber(uint16(v))
 	}
 	// encode KeyAuths as map[PubKey]uint16
 	enc.EncodeUVarint(uint64(len(auth.KeyAuths)))
-	keys := []string{}
-	for key := range auth.KeyAuths {
-		keys = append(keys, key)
-	}
 	for k, v := range auth.KeyAuths {
 		enc.EncodePubKey(k)
 		enc.EncodeNumber(uint16(v))
